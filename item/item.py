ab1d4f9df42a484f15ae96f4632b6cd406ecd729
Stephen Paul Brown <brownstephenp@gmail.com>
2016-04-28 13:35:39 -0700

    Updated item.py

--- item/item.py
+++ item/item.py
@@ -8,10 +8,14 @@ import datetime
 urls = (
         '/', 'Index',
         '/new', 'New',
+        '/newbid/(\d+)', 'New_Bid',
         '/item/(\d+)', 'Item',
         '/searchid', 'Search_ID',
         '/searchdesc', 'Search_Desc',
         '/searchcat', 'Search_Cat',
+        '/searchbid', 'Search_Bid',
+        '/searchED', 'Search_ED',
+        '/searchopen', 'Search_OPEN',
         )
 
 
@@ -45,15 +49,48 @@ class New:
         form.buyPrice = float(form.buyPrice)
         form.startDate = bool(form.startDate)
         # form.endDate = datetime('now')
-        model.new_item(form.Category, form.Title, form.Description, float(form.buyPrice), form.startDate, form.endDate)
+
+        result = model.new_item(form.Category, form.Title, form.Description, float(form.buyPrice), form.startDate, form.endDate)
+        print("result")
+        print(result)
+        model.place_bid(result, float(0), "No current bids")
         raise web.seeother('/')
+
+class New_Bid:
+    
+    form = web.form.Form()
+
+    def POST(self, id):
+        """ Add new bid """
+        print("In New_Bid")
+        form = web.input()
+
+        try:
+           form.bid = float(form.bid)
+        except:
+           raise web.seeother("/")
+        print(form)
+        print(form.user)
+        print(form.bid)
+        print(id)
+
+        print("Here")
+        model.place_bid(id, form.bid, form.user)
+        item = model.get_one_item(id)
+        bid = model.get_bid(id)
+
+        return render.item(item, bid)
+
 
 class Item:
     
     def GET(self, id):
         """ View single post """
         item = model.get_one_item(int(id))
-        return render.item(item)
+        bid = model.get_bid(int(id)) 
+        print(item)
+        print(bid)
+        return render.item(item, bid)
 
 
 class Search_ID:
@@ -111,6 +148,67 @@ class Search_Cat:
 
         print(sform.id) # Print value
         items = model.search_cat(sform.id)
+
+        print(items)
+
+        form = self.form()
+        return render.index(items, form)
+
+class Search_Bid:
+    print("In Search")
+    form = web.form.Form()
+
+    def GET(self):
+        """ Show page """
+        print("In GET")
+        try:
+            sform = web.input()
+            sform.id = float(sform.id)
+        except:
+            raise web.seeother('/')
+
+        print(sform.id) # Print value
+        items = model.search_bid(sform.id)
+
+        print(items)
+
+        form = self.form()
+        return render.index(items, form)
+
+class Search_ED:
+    print("In Search")
+    form = web.form.Form()
+
+    def GET(self):
+        """ Show page """
+        print("In GET")
+
+        sform = web.input()
+        if sform.id == '':
+            raise web.seeother('/')
+
+        print(sform.id) # Print value
+        items = model.search_ed(sform.id)
+
+        print(items)
+
+        form = self.form()
+        return render.index(items, form)
+
+class Search_OPEN:
+    print("In Search")
+    form = web.form.Form()
+
+    def GET(self):
+        """ Show page """
+        print("In GET")
+
+        sform = web.input()
+        if bool(sform.id) == 0:
+            raise web.seeother('/')
+
+        print(sform.id) # Print value
+        items = model.search_open(sform.id)
 
         print(items)
 
